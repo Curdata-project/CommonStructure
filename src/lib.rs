e31237631b8d1e5ac5758b7a25b91a29b0ff3886
xujian <s1473561555@sina.com>
2020-06-29 23:25:37 -0400

    remove thread_rng, replace with OsRng

--- src/lib.rs
+++ src/lib.rs
@@ -20,39 +20,8 @@ pub enum TransactionError {
     SystemError,
 }
 
-use alloc::vec;
-use alloc::vec::Vec;
-use lazy_static::*;
-
-use rand::Error;
 use rand::RngCore;
-use getrandom::getrandom;
 
 pub fn get_rng_core() -> impl RngCore + Send {
-    GetRandomRng{}
-}
-
-#[derive(Default)]
-struct GetRandomRng{}
-
-impl RngCore for GetRandomRng {
-    fn next_u32(&mut self) -> u32 {
-        let mut tmp = [0u8; 4];
-        getrandom(&mut tmp).unwrap();
-        u32::from_le_bytes(tmp)
-    }
-
-    fn next_u64(&mut self) -> u64 {
-        let mut tmp = [0u8; 8];
-        getrandom(&mut tmp).unwrap();
-        u64::from_le_bytes(tmp)
-    }
-
-    fn fill_bytes(&mut self, dest: &mut [u8]) {
-        getrandom(dest).unwrap();
-    }
-
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
-        Ok(self.fill_bytes(dest))
-    }
+    rand::rngs::OsRng
 }
